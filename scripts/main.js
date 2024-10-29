3db6ebac2e781e5cba070e0bfa2f922a1a0b0a2b
yddobyb <139486122+yddobyb@users.noreply.github.com>
2024-10-29 11:47:46 -0700

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,43 @@
-function getNameFromAuth() {
-    firebase.auth().onAuthStateChanged(user => {
-        // Check if a user is signed in:
-        if (user) {
-            // Do something for the currently logged-in user here: 
-            console.log(user.uid); //print the uid in the browser console
-            console.log(user.displayName);  //print the user name in the browser console
-            userName = user.displayName;
+// Function to read the quote of the day from the Firestore "quotes" collection
+// Input param is the String representing the day of the week, aka, the document name
+function readQuote(day) {
+    db.collection("quotes").doc(day)                                                         //name of the collection and documents should matach excatly with what you have in Firestore
+        .onSnapshot(dayDoc => {                                                              //arrow notation
+            console.log("current document data: " + dayDoc.data());                          //.data() returns data object
+            document.getElementById("quote-goes-here").innerHTML = dayDoc.data().quote;      //using javascript to display the data on the right place
 
-            //method #1:  insert with JS
-            document.getElementById("name-goes-here").innerText = userName;
+            //Here are other ways to access key-value data fields
+            // $('#quote-goes-here').text(dayDoc.data().quote);         //using jquery object dot notation
+            // $("#quote-goes-here").text(dayDoc.data()["quote"]);      //using json object indexing
+            // document.querySelector("#quote-goes-here").innerHTML = dayDoc.data().quote;
 
-            //method #2:  insert using jquery
-            //$("#name-goes-here").text(userName); //using jquery
-
-            //method #3:  insert using querySelector
-            //document.querySelector("#name-goes-here").innerText = userName
-
-        } else {
-            // No user is signed in.
-            console.log("No user is logged in");
-        }
-    });
+        }, (error) => {
+            console.log("Error calling onSnapshot", error);
+        });
 }
-getNameFromAuth(); //run the function
+readQuote("tuesday");
+
+
+
+function insertNameFromFirestore() {
+    // Check if the user is logged in:
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            console.log(user.uid); // Let's know who the logged-in user is by logging their UID
+            currentUser = db.collection("users").doc(user.uid); // Go to the Firestore document of the user
+            currentUser.get().then(userDoc => {
+                // Get the user name
+                let userName = userDoc.data().name;
+                console.log(userName);
+                //$("#name-goes-here").text(userName); // jQuery
+                document.getElementById("name-goes-here").innerText = userName;
+            })
+        } else {
+            console.log("No user is logged in."); // Log a message when no user is logged in
+        }
+    })
+}
+insertNameFromFirestore();
+
+
+//calling the function
